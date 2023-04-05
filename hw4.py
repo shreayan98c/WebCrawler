dc849155ee7145d607d87d644f89a990258e1661
shreayan98c <shreayan98c@gmail.com>
2023-04-04 20:51:01 -0400

    Info extraction

--- hw4.py
+++ hw4.py
@@ -2,7 +2,7 @@ import logging
 import re
 import sys
 from bs4 import BeautifulSoup
-from queue import Queue
+from queue import Queue, PriorityQueue
 from urllib import parse, request
 
 logging.basicConfig(level=logging.DEBUG, filename='output.log', filemode='w')
@@ -22,9 +22,28 @@ def parse_links(root, html):
             yield (parse.urljoin(root, link.get('href')), text)
 
 
-def parse_links_sorted(root, html):
+def relevance_func(link, key_w):
+    if len(key_w) == 0:
+        return 0
+    for w in key_w:
+        if w in link:
+            return 0
+    else:
+        return 1
+
+
+def parse_links_sorted(root, html, key_w):
     # TODO: implement
-    return []
+    soup = BeautifulSoup(html, 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            text = link.string
+            if not text:
+                text = ''
+            text = re.sub('\s+', ' ', text).strip()
+            url = parse.urljoin(root, link.get('href'))
+            yield ((relevance_func(url, key_w), url), text)
 
 
 def get_links(url):
@@ -40,37 +59,49 @@ def get_nonlocal_links(url):
     # TODO: implement
     links = get_links(url)
     filtered = []
+    for link in links:
+        if parse.urlparse(link[0]).hostname != parse.urlparse(url).hostname:
+            filtered.append(link)
     return filtered
 
 
-def crawl(root, wanted_content=[], within_domain=True):
+def crawl(root, wanted_content=[], within_domain=True, num_link=10, key_w=[]):
     '''Crawl the url specified by `root`.
     `wanted_content` is a list of content types to crawl
     `within_domain` specifies whether the crawler should limit itself to the domain of `root`
     '''
     # TODO: implement
 
-    queue = Queue()
-    queue.put(root)
+    queue = PriorityQueue()
+    queue.put((relevance_func(root, key_w), root))
 
-    visited = []
+    visited = set()
     extracted = []
 
     while not queue.empty():
-        url = queue.get()
+        if len(visited) > num_link:
+            break
+        rank, url = queue.get()
         try:
             req = request.urlopen(url)
+            wanted_content = [x.lower() for x in wanted_content]
+            content = req.headers['Content-Type'].lower()
+            if wanted_content and (content not in wanted_content):
+                continue
             html = req.read()
 
-            visited.append(url)
+            visited.add(url)
             visitlog.debug(url)
 
             for ex in extract_information(url, html):
                 extracted.append(ex)
                 extractlog.debug(ex)
 
-            for link, title in parse_links(url, html):
-                queue.put(link)
+            for (rank, link), title in parse_links_sorted(url, html, key_w):
+                if (link in visited) or (parse.urlparse(link) == parse.urlparse(root)) or (
+                        within_domain and parse.urlparse(link).hostname != parse.urlparse(url).hostname):
+                    continue
+                queue.put((rank, link))
 
         except Exception as e:
             print(e, url)
@@ -86,6 +117,13 @@ def extract_information(address, html):
     results = []
     for match in re.findall('\d\d\d-\d\d\d-\d\d\d\d', str(html)):
         results.append((address, 'PHONE', match))
+
+    for match in re.findall(r"([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+)", str(html)):
+        results.append((address, 'EMAIL', match))
+
+    for match in re.findall(r'([A-Z]([a-z])+(\.?)(\x20[A-Z]([a-z])+){0,2},\s[A-Za-z]+\.{0,1}\s(?!0{5})\d{5})',
+                            str(html)):
+        results.append((address, 'ADDRESS', match[0]))
     return results
 
 
